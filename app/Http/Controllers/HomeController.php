4cece1345e37dd12db0ee6bf87194c77768d446e
Zaid Aly <zaidk4076@gmail.com>
2024-11-02 18:45:27 +0530

    constaints used in blade file

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index(){
+        return view('home.index')
+        ->with('fname', 'Syed')
+        ->with('mname', 'zain')
+        ->with('lname', 'aly');
+    }
+}
